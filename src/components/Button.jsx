b356ef0aa75b3e2e1fa47355a68742ed2f4032a2
ZeraiGR <kirillgr15@mail.ru>
2022-09-06 20:03:47 +0300

    the first preparing

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Button = styled.button`
+  background-color: var(--accent);
+  font-size: 1.5rem;
+  padding: 0.25em 1em;
+  border-radius: 10px;
+`;
---
+++ src/layout/Header.jsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  background-color: var(--accent);
+  padding: 1.625rem 2.5rem;
+`;
